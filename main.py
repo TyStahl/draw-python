391508c513f1e49ca56ed494cbe8451a88e0a2be
Nikita Tsvetkov <nikitat1994@gmail.com>
2025-03-29 22:05:00 -0700

    basic rectangle drawing

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
+import pygame
+
 def main():
-    print("Hello World!")
+    pygame.init()
+    screen_x = 1280
+    screen_y = 720
+    screen = pygame.display.set_mode((screen_x, screen_y))
+    clock = pygame.time.Clock()
+    running = True
+    
+    mouse_is_down = False
+
+    screen.fill((255,255,255))
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            print(event)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_is_down = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_is_down = False
+            if event.type == pygame.MOUSEMOTION:
+                if mouse_is_down:
+                    pos = pygame.mouse.get_pos()
+                    pygame.draw.rect(screen, (0,0,8), (pos[0], pos[1], 10, 10))
+            if event.type == pygame.QUIT:
+                running = False
+
+        
+        pygame.display.flip()
+        clock.tick(60)  # limits FPS to 60
+
+    pygame.quit()
 
 main()
